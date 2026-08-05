e0646b7338eb616a1e7056c4b7c6b211187bd673
Mohit Laicha <mohitlaicha22@gmail.com>
2026-08-05 20:53:39 +1000

    Configure frontend deployment

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+
 
 export const api = axios.create({
   baseURL: API_URL,
 });
 
-
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "http://localhost:5000/api";
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
